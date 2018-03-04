69e1c53c9af0f3eea48dfa7560e28907bd39728c
Takaaki Kumazawa <30303702+takaakikumazawa@users.noreply.github.com>
2018-03-04 20:21:27 +1100

    Project Merge
    -Added prototype base level
    -Added prototype basic player control

---
+++ Source/GPG220_Project/Private/MainCharacter.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MainCharacter.h"
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
+
+// Sets default values
+AMainCharacter::AMainCharacter()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CreateDefaultSubobject<UFloatingPawnMovement>("PawnMovement");
+
+	CubeMesh = CreateDefaultSubobject<UStaticMeshComponent>("CubeMesh");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+}
+
+// Called when the game starts or when spawned
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMainCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMainCharacter::MoveRight);
+}
+
+void AMainCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+void AMainCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
